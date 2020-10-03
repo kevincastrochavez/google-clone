6c1c67dc571e13d6afb577027b2ccd9cb7a13fa4
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-10-02 20:45:53 -0500

    setup react-router-dom, material-ui and created some links in the home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import "./styles/main.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Google Clone</h1>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          {/* <Route path="/search" component={} /> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
