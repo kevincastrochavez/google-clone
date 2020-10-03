9e9155c8d8af03ffa010ccd5524ac60637a84f84
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-10-02 22:29:26 -0500

    connected input on search bar to the state

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
 
 function Search() {
+  const [input, setInput] = useState("");
+
+  const search = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input />
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
         <MicIcon />
+      </div>
+
+      <div className="search__buttons">
+        <Button onClick={search} variant="outlined">
+          Search for Something
+        </Button>
+        <Button variant="outlined">Look</Button>
       </div>
     </div>
   );
