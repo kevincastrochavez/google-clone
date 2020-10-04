bfeaddc44464bf6ffd9d5c18bfe794eef63820a8
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-10-04 14:02:05 -0500

    got rid of console.log for deployment

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -14,8 +14,6 @@ function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
   const { data } = useGoogleSearch(term);
 
-  console.log(data);
-
   return (
     <div className="searchPage">
       <div className="searchPage__header">
